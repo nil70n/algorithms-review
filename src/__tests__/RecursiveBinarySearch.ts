03f0910eb7c4d84ec0f9975c40f202dd4a40558b
Nilton Nascimento <nilton.nascimento@kehe.com>
2023-11-01 18:01:51 -0300

    Fix formatting

--- src/__tests__/RecursiveBinarySearch.ts
+++ src/__tests__/RecursiveBinarySearch.ts
@@ -2,18 +2,18 @@ import recursive_binary_search from "@code/RecursiveBinarySearch";
 
 test("Binary Search", function () {
   expect(
-    recursive_binary_search ([1,3,5,8,10,11,15,22,38,40,44], 40))
-    .toEqual(true);
+    recursive_binary_search([1, 3, 5, 8, 10, 11, 15, 22, 38, 40, 44], 40),
+  ).toEqual(true);
 
   expect(
-    recursive_binary_search ([1,3,5,8,10,11,15,22,38,40,44], 1))
-    .toEqual(true);
+    recursive_binary_search([1, 3, 5, 8, 10, 11, 15, 22, 38, 40, 44], 1),
+  ).toEqual(true);
 
   expect(
-    recursive_binary_search ([1,3,5,8,10,11,15,22,38,40,44], 0))
-    .toEqual(false);
+    recursive_binary_search([1, 3, 5, 8, 10, 11, 15, 22, 38, 40, 44], 0),
+  ).toEqual(false);
 
   expect(
-    recursive_binary_search ([1,3,5,8,10,11,15,22,38,40,44], 2))
-    .toEqual(false);
+    recursive_binary_search([1, 3, 5, 8, 10, 11, 15, 22, 38, 40, 44], 2),
+  ).toEqual(false);
 });
--- src/__tests__/SelectionSort.ts
+++ src/__tests__/SelectionSort.ts
@@ -1,7 +1,7 @@
 import selection_sort from "@code/SelectionSort";
 
 test("Selection Sort", function () {
-  expect(
-    selection_sort ([20,18,50,11,125,4]))
-    .toEqual([4,11,18,20,50,125]);
+  expect(selection_sort([20, 18, 50, 11, 125, 4])).toEqual([
+    4, 11, 18, 20, 50, 125,
+  ]);
 });
